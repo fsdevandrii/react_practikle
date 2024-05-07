855916522da5139ce15b41e04d4751c6061d330e
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2024-05-07 14:57:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -6,9 +6,35 @@ import { Users } from "./components/Users";
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  React.useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей!");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
 
       {/* <Success /> */}
     </div>
