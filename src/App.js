52d4027af748f97a4b71ff9a2c2b874ba753c715
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2024-05-08 22:42:45 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch(`https://cdn.cur.su/api/latest.json`)
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+        console.log(json.rates);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Не удалось получить информацию!");
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
       <Block value={0} currency="USD" />
     </div>
   );
